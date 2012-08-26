e79846eeb303663dfb27ecd5707a85234eb57ae8
Sidharth Shanker <sshanker220@gmail.com>
2012-08-25 22:07:55 -0700

    Added proper column_family methods

--- lib/column_family.rb
+++ lib/column_family.rb
@@ -2,8 +2,13 @@ module Cassandruby
   module ColumnFamily
     module ColumnFamilyClassMethods
       def field(name, options = {})
-        @fields = {} if @fields.nil?
-        @fields[name] = true 
+        define_method name.to_sym do
+          instance_variable_get(":@#{name}")
+        end
+      
+        define_method "#{name}=".to_sym do |val|
+          instance_variable_set(":@#{name}", val)
+        end 
       end 
     end 
     
