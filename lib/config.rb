9accf9d49c84e14caf6fd0432cdd79091958caae
Sidharth Shanker <sshanker220@gmail.com>
2012-08-25 20:17:51 -0700

    metaprogram config

--- lib/config.rb
+++ lib/config.rb
@@ -3,19 +3,15 @@ require 'pry'
 module Cassandruby
   module Config
     extend self
+  
+    %w(keyspace host port).each do |meth|
+      define_method "#{meth}=".to_sym do |param|
+        self.instance_variable_set("@#{meth}".to_sym,param)
+      end
     
-    binding.pry
-
-    def keyspace=(keyspace)
-      @keyspace = keyspace 
-    end
-
-    def host=(host)
-      @host = host
-    end
-
-    def port=(port)
-      @port = port
+      define_method meth.to_sym do
+        instance_variable_get("@#{meth}".to_sym)
+      end
     end  
   end
 end
